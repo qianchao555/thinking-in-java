fa5001862d52bdad6590fe42e3511b2eca52338e
chao.qian <chao.qian@tinno.com>
2019-10-21 10:49:49 +0800

    transient type commit

---
+++ src/com/qc/ch18/LogonTransient.java
@@ -0,0 +1,21 @@
+package com.qc.ch18;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ProjectName: ThinkingInJava
+ * @Package: com.qc.ch18
+ * @ClassName: LogonTransient
+ * @Author: android2
+ * @Description:
+ * @Date: 2019/10/21 上午10:48
+ * @Version: 1.0
+ */
+public class LogonTransient implements Serializable {
+    private Date date=new Date();
+
+    public static void main(String[] args) {
+        
+    }
+}
