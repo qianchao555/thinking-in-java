6d4ad204997505939a376b4c788de549c8a24b6d
chao.qian <chao.qian@tinno.com>
2019-10-21 11:02:12 +0800

    transient type commit

--- src/com/qc/ch18/LogonTransient.java
+++ src/com/qc/ch18/LogonTransient.java
@@ -1,7 +1,8 @@
 package com.qc.ch18;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ProjectName: ThinkingInJava
@@ -13,9 +14,30 @@ import java.util.Date;
  * @Version: 1.0
  */
 public class LogonTransient implements Serializable {
-    private Date date=new Date();
+    private Date date = new Date();
+    private String username;
+    private transient String password;
 
-    public static void main(String[] args) {
-        
+    public LogonTransient(String name, String pwd) {
+        this.username = name;
+        this.password = pwd;
+    }
+    @Override
+    public String toString(){
+        return "username:"+username+"\npassword:"+password+" \n"+ date;
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        LogonTransient a = new LogonTransient("qc", "110110");
+        System.out.println(a);
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(new FileOutputStream("Logon.out"));
+        objectOutputStream.writeObject(a);
+        objectOutputStream.close();
+        TimeUnit.SECONDS.sleep(2);
+        ObjectInputStream in=new ObjectInputStream(new FileInputStream("Logon.out"));
+        System.out.println("Recovering object at "+new Date());
+        a= (LogonTransient) in.readObject();
+        System.out.println(a);
     }
 }
